d152060bf1d20bbd18aa0a00b6ceb3d0865a77f8
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-15 18:45:43 -0700

    debugged

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function lightMode() {
   textBox.style.backgroundColor = 'rgb(0 0 0 / 50%';
   toggleIcon.children[0].textContent = 'Light Mode';
   toggleIcon.children[1].classList.remove('fa-moon');
-  toggleIcon.children[1].classList.add('fa-sun`');
+  toggleIcon.children[1].classList.add('fa-sun');
   image1.src = 'img/undraw_proud_coder_light.svg';
   image2.src = 'img/undraw_feeling_proud_light.svg';
   image3.src = 'img/undraw_conceptual_idea_light.svg';
